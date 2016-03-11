db3666e96582f87f895d06b66909c298777da3d7
Andy <rym1112ov@gmail.com>
2016-03-11 14:15:00 +0200

    add styles

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var btn = document.getElementById('btn');
 var ult = document.getElementById('das');
-    
+ var p = document.getElementById('text');   
   
 
 btn.addEventListener("click",function(){
@@ -12,11 +12,21 @@ btn.addEventListener("click",function(){
   li.classList.toggle("das__item");
   span.classList.toggle("das__cross");
   span.innerHTML = "&times";
-  span.onclick = function() {ult.removeChild(li);}
-  ult.appendChild(li); 
+
+  span.onclick = function() {ult.removeChild(li);};
+  if(document.getElementById('inp').value == ''){p.innerHTML = "ошибка,введите текст!";
+  p.style.display = "block";
+
+  }
+  else{ult.appendChild(li);
+  	}
+   
+
   document.getElementById('inp').value = ''; 
 });
-
+document.getElementById('inp').addEventListener("click",function(){
+  	p.style.display = "none";
+  }); 
 var x = ult.children;
 for (var i = 0; i < x.length; i++){
     x[i].addEventListener("click",function(){
